633af8d0eb0688da3dcb78391f5d99152efe99b3
yuxng <xyfudan@gmail.com>
2018-10-16 21:50:52 -0700

    ycb subset

--- experiments/scripts/ycb_video_train_subset.sh
+++ experiments/scripts/ycb_video_train_subset.sh
@@ -4,9 +4,9 @@ set -x
 set -e
 
 export PYTHONUNBUFFERED="True"
-#export CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=0,3
 
-LOG="experiments/logs/ycb_video_train.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+LOG="experiments/logs/ycb_video_train_subset.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
@@ -14,7 +14,7 @@ time ./tools/train_net.py \
   --network posecnn \
   --pretrained data/checkpoints/vgg16-397923af.pth \
   --dataset ycb_video_train \
-  --cfg experiments/cfgs/ycb_video.yml \
+  --cfg experiments/cfgs/ycb_video_subset.yml \
   --cad data/YCB_Video/models.txt \
   --pose data/YCB_Video/poses.txt \
   --solver sgd \
