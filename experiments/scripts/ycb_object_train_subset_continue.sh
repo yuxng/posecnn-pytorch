517ba8a1c02f5c5c6990af231b1373e2fe0b99d6
yuxng <xyfudan@gmail.com>
2018-11-23 16:05:36 -0800

    test mug

--- experiments/scripts/ycb_object_train_subset_continue.sh
+++ experiments/scripts/ycb_object_train_subset_continue.sh
@@ -12,11 +12,9 @@ echo Logging output to "$LOG"
 
 time ./tools/train_net.py \
   --network posecnn \
-  --pretrained output/ycb_object/ycb_object_train/vgg16_ycb_object_pose_epoch_12.checkpoint.pth \
-  --startepoch 12 \
+  --pretrained output/ycb_object/ycb_object_train/vgg16_ycb_object_pose_epoch_3.checkpoint.pth \
+  --startepoch 3 \
   --dataset ycb_object_train \
   --cfg experiments/cfgs/ycb_object_subset.yml \
-  --cad data/YCB_Video/models.txt \
-  --pose data/YCB_Video/poses.txt \
   --solver sgd \
   --epochs 16
