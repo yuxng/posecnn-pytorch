0efb609f63362cec917810ded3e5c2f364d7cba0
yuxng <xyfudan@gmail.com>
2018-12-10 16:38:37 -0800

    local

--- experiments/scripts/ycb_object_train_subset.sh
+++ experiments/scripts/ycb_object_train_subset.sh
@@ -4,14 +4,15 @@ set -x
 set -e
 
 export PYTHONUNBUFFERED="True"
-export CUDA_VISIBLE_DEVICES=0,1
+#export CUDA_VISIBLE_DEVICES=0,1
 export PANGOLIN_WINDOW_URI="headless://"
+export PYTHON_EGG_CACHE=/nfs
 
-LOG="experiments/logs/ycb_object_train_subset.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
-exec &> >(tee -a "$LOG")
-echo Logging output to "$LOG"
+#LOG="experiments/logs/ycb_object_train_subset.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+#exec &> >(tee -a "$LOG")
+#echo Logging output to "$LOG"
 
-time ./tools/train_net.py \
+./tools/train_net.py \
   --network posecnn \
   --pretrained data/checkpoints/vgg16-397923af.pth \
   --dataset ycb_object_train \
