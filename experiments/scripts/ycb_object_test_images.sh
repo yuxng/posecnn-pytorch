a99e4b9f252a4fe450ab311601327078cfd03ad2
yuxng <xyfudan@gmail.com>
2018-12-12 15:01:24 -0800

    add test images

--- experiments/scripts/ycb_object_test_images.sh
+++ experiments/scripts/ycb_object_test_images.sh
@@ -6,11 +6,12 @@ set -e
 export PYTHONUNBUFFERED="True"
 export CUDA_VISIBLE_DEVICES=$1
 
-LOG="experiments/logs/ros_ycb_object_test_subset.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+LOG="experiments/logs/ycb_object_test_images.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
-time ./ros/test_images.py --gpu 0 \
+time ./tools/test_images.py --gpu 0 \
+  --imgdir data/Images/video7 \
   --network posecnn \
   --pretrained output/ycb_object/ycb_object_train/vgg16_ycb_object_pose_dgx_1_epoch_7.checkpoint.pth \
   --dataset ycb_object_test \
