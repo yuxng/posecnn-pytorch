1a9d722c89c288681e2f5b8b5fe30945d9ee55af
yuxng <xyfudan@gmail.com>
2018-12-13 10:56:49 -0800

    test new models

--- experiments/scripts/ros_ycb_object_test_subset.sh
+++ experiments/scripts/ros_ycb_object_test_subset.sh
@@ -12,7 +12,7 @@ echo Logging output to "$LOG"
 
 time ./ros/test_images.py --gpu 0 \
   --network posecnn \
-  --pretrained output/ycb_object/ycb_object_train/vgg16_ycb_object_pose_dgx_1_epoch_7.checkpoint.pth \
+  --pretrained output/ycb_object/ycb_object_train/vgg16_ycb_object_pose_dgx_1_epoch_8.checkpoint.pth \
   --dataset ycb_object_test \
   --cfg experiments/cfgs/ycb_object_subset.yml \
   --cad data/YCB_Video/models.txt \
