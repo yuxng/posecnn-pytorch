9fefdc0d9f361ef379df95eb61fe5a271949a3b9
yuxng <xyfudan@gmail.com>
2018-12-21 09:20:41 -0800

    test sim

--- experiments/scripts/ros_ycb_object_test_subset_kinect2.sh
+++ experiments/scripts/ros_ycb_object_test_subset_kinect2.sh
@@ -12,7 +12,7 @@ echo Logging output to "$LOG"
 
 time ./ros/test_images.py --gpu 0 \
   --network posecnn \
-  --pretrained output/ycb_object/ycb_object_train/vgg16_ycb_object_pose_dgx_1_epoch_6.checkpoint.pth \
+  --pretrained output/ycb_object/ycb_object_train/vgg16_ycb_object_pose_dgx_2_epoch_5.checkpoint.pth \
   --dataset ycb_object_test \
   --cfg experiments/cfgs/ycb_object_subset_kinect2.yml \
   --cad data/YCB_Video/models.txt \
