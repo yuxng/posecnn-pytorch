596d5b820610856b7cd9c05ec11bd56e83179809
yuxng <xyfudan@gmail.com>
2019-01-02 10:27:36 -0800

    add dgx scripts

--- experiments/scripts/ycb_object_train_subset_dgx_1.sh
+++ experiments/scripts/ycb_object_train_subset_dgx_1.sh
@@ -5,15 +5,12 @@ set -e
 
 export PYTHONUNBUFFERED="True"
 export CUDA_VISIBLE_DEVICES=0,1
+export PYTHON_EGG_CACHE=/nfs
 
-LOG="experiments/logs/ycb_object_train_subset.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
-exec &> >(tee -a "$LOG")
-echo Logging output to "$LOG"
-
-time ./tools/train_net.py \
+./tools/train_net.py \
   --network posecnn \
   --pretrained data/checkpoints/vgg16-397923af.pth \
   --dataset ycb_object_train \
-  --cfg experiments/cfgs/ycb_object_subset.yml \
+  --cfg experiments/cfgs/ycb_object_subset_dgx_1.yml \
   --solver sgd \
   --epochs 16
