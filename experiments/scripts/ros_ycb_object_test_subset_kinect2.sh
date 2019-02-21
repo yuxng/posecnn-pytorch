0b650f8086b31f135de5d55e040b86a932a527ca
yuxng <xyfudan@gmail.com>
2019-02-20 16:58:50 -0800

    new scripts

--- experiments/scripts/ros_ycb_object_test_subset_kinect2.sh
+++ experiments/scripts/ros_ycb_object_test_subset_kinect2.sh
@@ -4,13 +4,13 @@ set -x
 set -e
 
 export PYTHONUNBUFFERED="True"
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=$1
 
 LOG="experiments/logs/ros_ycb_object_test_subset.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
-time ./ros/test_images.py --gpu 0 \
+time ./ros/test_images.py --gpu $1 \
   --instance $1 \
   --network posecnn \
   --pretrained output/ycb_object/ycb_object_train/vgg16_ycb_object_pose_dgx_2_epoch_16.checkpoint.pth \
