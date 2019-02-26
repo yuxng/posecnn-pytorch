56f6be1bdd5764df1775a073e221a3a9a58178c4
yuxng <xyfudan@gmail.com>
2019-02-26 09:44:15 -0800

    add more scripts

--- experiments/scripts/ros_ycb_object_test_subset.sh
+++ experiments/scripts/ros_ycb_object_test_subset.sh
@@ -13,6 +13,6 @@ echo Logging output to "$LOG"
 time ./ros/test_images.py --gpu $1 \
   --instance $2 \
   --network posecnn \
-  --pretrained output/ycb_object/ycb_object_train/vgg16_ycb_object_pose_dgx_2_epoch_5.checkpoint.pth \
+  --pretrained output/ycb_object/ycb_object_train/vgg16_ycb_object_pose_dgx_2_epoch_16.checkpoint.pth \
   --dataset ycb_object_test \
   --cfg experiments/cfgs/ycb_object_subset_dgx_2.yml
