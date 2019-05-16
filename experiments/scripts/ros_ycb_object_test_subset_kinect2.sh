e23e622660fe8b169269199330f1d029b8435cad
yuxng <xyfudan@gmail.com>
2019-05-16 16:37:14 -0700

    new kinect scripts

--- experiments/scripts/ros_ycb_object_test_subset_kinect2.sh
+++ experiments/scripts/ros_ycb_object_test_subset_kinect2.sh
@@ -11,10 +11,8 @@ exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
 time ./ros/test_images.py --gpu $1 \
-  --instance $1 \
+  --instance $2 \
   --network posecnn \
-  --pretrained output/ycb_object/ycb_object_train/vgg16_ycb_object_pose_dgx_2_epoch_16.checkpoint.pth \
+  --pretrained output/ycb_object/ycb_object_train/vgg16_ycb_object_pose_dgx_2_epoch_16_5objects.checkpoint.pth \
   --dataset ycb_object_test \
-  --cfg experiments/cfgs/ycb_object_subset_dgx_2.yml \
-  --cad data/YCB_Video/models.txt \
-  --pose data/YCB_Video/poses.txt
+  --cfg experiments/cfgs/ycb_object_subset_kinect2.yml
