8fedf01795285f0ce7e7fcaebf0b9b442ef0e228
yuxng <xyfudan@gmail.com>
2019-07-29 11:56:19 -0700

    add test ycb video tuna can

---
+++ experiments/scripts/ycb_video_test_tuna_can.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x
+set -e
+
+export PYTHONUNBUFFERED="True"
+export CUDA_VISIBLE_DEVICES=$1
+
+LOG="experiments/logs/ycb_object_flow_test.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+exec &> >(tee -a "$LOG")
+echo Logging output to "$LOG"
+
+time ./tools/test_net.py --gpu $1 \
+  --network posecnn \
+  --pretrained output/ycb_object/ycb_object_train/vgg16_ycb_object_slim_epoch_16.checkpoint.pth \
+  --dataset ycb_video_keyframe \
+  --cfg experiments/cfgs/ycb_object_subset.yml
