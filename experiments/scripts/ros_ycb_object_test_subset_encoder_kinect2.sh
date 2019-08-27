998fc1529923abe563704c58898ce1bf7d294977
yuxng <xyfudan@gmail.com>
2019-08-27 14:37:02 -0700

    add more scripts

---
+++ experiments/scripts/ros_ycb_object_test_subset_encoder_kinect2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+	
+set -x
+set -e
+
+export PYTHONUNBUFFERED="True"
+export CUDA_VISIBLE_DEVICES=$1
+
+LOG="experiments/logs/ros_ycb_object_test_subset.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+exec &> >(tee -a "$LOG")
+echo Logging output to "$LOG"
+
+time ./ros/test_images_D435.py --gpu $1 \
+  --instance $2 \
+  --network posecnn \
+  --pretrained data/checkpoints/vgg16_ycb_object_self_supervision_epoch_8.checkpoint.pth \
+  --dataset ycb_object_test \
+  --cfg experiments/cfgs/ycb_object_self_supervision_kinect2.yml
