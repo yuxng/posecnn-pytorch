464a336d01074dc531a0fed58bddf8b876b3af49
yuxng <xyfudan@gmail.com>
2019-08-30 16:45:15 -0700

    train self supervision

--- experiments/scripts/ycb_object_test_images.sh
+++ experiments/scripts/ycb_object_test_images.sh
@@ -11,8 +11,8 @@ exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
 time ./tools/test_images.py --gpu $1 \
-  --imgdir data/Images/kitchen2 \
+  --imgdir data/Images/kitchen3 \
   --network posecnn \
-  --pretrained output/ycb_object/ycb_object_train/vgg16_ycb_object_slim_dope_bowl_epoch_2.checkpoint.pth \
+  --pretrained output/ycb_self_supervision/ycb_self_supervision_train/vgg16_ycb_object_self_supervision_epoch_16.checkpoint.pth \
   --dataset ycb_self_supervision_test \
-  --cfg experiments/cfgs/ycb_object_dope_bowl.yml
+  --cfg experiments/cfgs/ycb_object_self_supervision.yml
