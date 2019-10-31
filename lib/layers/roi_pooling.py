f6e2aec53699a7ffe37a1ac2b03d0ac154b100be
yuxng <yux@nvidia.com>
2019-10-31 15:51:36 -0700

    static for roi pooling

--- lib/layers/roi_pooling.py
+++ lib/layers/roi_pooling.py
@@ -6,28 +6,26 @@ import posecnn_cuda
 
 
 class RoIPoolFunction(Function):
-    def __init__(self, pool_height, pool_width, spatial_scale):
-        self.pool_width = int(pool_width)
-        self.pool_height = int(pool_height)
-        self.spatial_scale = float(spatial_scale)
-        self.rois = None
-        self.feature_size = None
-        self.argmax_data = None
-
-    def forward(self, features, rois):
-        self.rois = rois
-        self.feature_size = features.size()
-
-        outputs = posecnn_cuda.roi_pool_forward(self.pool_height, self.pool_width, self.spatial_scale, features, rois)
+    @staticmethod
+    def forward(ctx, features, rois, pool_height, pool_width, spatial_scale):
+        outputs = posecnn_cuda.roi_pool_forward(pool_height, pool_width, spatial_scale, features, rois)
         top_data = outputs[0]
-        self.argmax_data = outputs[1]
+        variables = outputs[1:]
+        variables.append(rois)
+        ctx.feature_size = features.size()
+        ctx.spatial_scale = spatial_scale
+        ctx.save_for_backward(*variables)
         return top_data
 
-    def backward(self, top_diff):
-        batch_size, num_channels, data_height, data_width = self.feature_size
-        outputs = posecnn_cuda.roi_pool_backward(batch_size, data_height, data_width, self.spatial_scale, top_diff, self.rois, self.argmax_data)
+    @staticmethod
+    def backward(ctx, top_diff):
+        argmax_data = ctx.saved_variables[0]
+        rois = ctx.saved_variables[1]
+        batch_size, num_channels, data_height, data_width = ctx.feature_size
+        spatial_scale = ctx.spatial_scale
+        outputs = posecnn_cuda.roi_pool_backward(batch_size, data_height, data_width, spatial_scale, top_diff, rois, argmax_data)
         d_features = outputs[0]
-        return d_features, None   
+        return d_features, None, None, None, None   
 
 class RoIPool(nn.Module):
     def __init__(self, pool_height, pool_width, spatial_scale):
@@ -38,5 +36,4 @@ class RoIPool(nn.Module):
         self.spatial_scale = float(spatial_scale)
 
     def forward(self, features, rois):
-        return RoIPoolFunction(self.pool_height, self.pool_width,
-                                self.spatial_scale)(features, rois)
+        return RoIPoolFunction.apply(features, rois, self.pool_height, self.pool_width, self.spatial_scale)
